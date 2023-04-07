7f030e48867f115dedf933314ab1b8216b3deef3
DonovanLight <130189307+DonovanLight@users.noreply.github.com>
2023-04-07 15:27:28 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,77 @@
+bluetooth.onBluetoothConnected(function () {
+    basic.showLeds(`
+        . # . # .
+        . # . # .
+        . . . . .
+        # . . . #
+        . # # # .
+        `)
+})
+gamepad.setStatusChangeHandler(function () {
+    if (gamepad._dpad(GameDirection.noDirection) == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # # # .
+            . . . . .
+            . . . . .
+            `)
+    } else if (gamepad._dpad(GameDirection.up) == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # # # .
+            # . # . #
+            . . # . .
+            . . # . .
+            `)
+    } else if (gamepad._dpad(GameDirection.down) == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . # . .
+            # . # . #
+            . # # # .
+            . . # . .
+            `)
+    } else if (gamepad._dpad(GameDirection.left) == 1) {
+        basic.showLeds(`
+            . . # . .
+            . # . . .
+            # # # # #
+            . # . . .
+            . . # . .
+            `)
+    } else if (gamepad._dpad(GameDirection.right) == 1) {
+        basic.showLeds(`
+            . . # . .
+            . . . # .
+            # # # # #
+            . . . # .
+            . . # . .
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            # # . . #
+            # . # . #
+            # . . # #
+            # . . . #
+            `)
+    }
+})
+basic.showLeds(`
+    # . # . #
+    # . # . .
+    # # # . #
+    # . # . #
+    # . # . #
+    `)
+bluetooth.startAccelerometerService()
+bluetooth.startButtonService()
+bluetooth.startIOPinService()
+bluetooth.startLEDService()
+bluetooth.startTemperatureService()
+bluetooth.startMagnetometerService()
+gamepad.startGamepadService()
+basic.forever(function () {
+	
+})
